d28a90d2fe763e7bbdc1403a6fa3e012d91ccee2
Enam <enamkumi@yahoo.com>
2013-07-15 22:11:58 -0700

    Computer science

---
+++ scripts/process_registration.php
@@ -0,0 +1,35 @@
+<?php
+$host="localhost";
+$user="root";
+$password="";
+
+@$connect=mysql_connect($host,$user,$password) or die(mysql_error());
+@$selectDB=mysql_select_db("cosaps") or die(mysql_error());
+
+
+//if(isset($_POST["fname"],$_POST['lname'],$_POST['mail'],$_POST['pass'],$_POST['no'])){
+
+
+//grab the form variables
+$firstname = mysql_real_escape_string($_GET["fname"]);
+$lastname = mysql_real_escape_string($_GET["lname"]);
+$email = mysql_real_escape_string($_GET["mail"]);
+$password = md5($_GET["pass"]);
+$number = $_GET["no"];
+
+
+
+$query = "INSERT INTO cosaps_basic_info
+		  
+		  (firstname,lastname,email,password,phone)
+		  
+		  VALUES
+		  
+		  ('$firstname','$lastname','$email','$password','$number')";
+
+$r = mysql_query($query) or die(mysql_error());
+
+
+//}
+   
+?>
